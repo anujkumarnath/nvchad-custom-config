fe9af3c4bea6ca1cee97b254242006ecd4e4e5a2
Anuj Kumar Nath <anujkumarnath@gmail.com>
2023-02-13 08:46:56 +0000

    Following NvChad docs to override options

--- init.lua
+++ init.lua
@@ -1,16 +1,22 @@
 local autocmd = vim.api.nvim_create_autocmd
+local opt = vim.opt
 
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
-vim.cmd([[
-	set notimeout
-	set number relativenumber
-]])
 
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.expandtab = false
+-- Numbers
+opt.relativenumber = true
+
+-- Indenting
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.expandtab = false
+opt.smartindent = true
+
+-- don't go to previous/next line with h,l,left arrow and right arrow
+-- when cursor reaches end/beginning of line
+opt.whichwrap = ""
